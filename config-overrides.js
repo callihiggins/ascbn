4be74e7b7e876693bb27aac9aa6579a7a22651a3
Calli <calli@nyu.edu>
2020-06-25 02:20:19 -0400

    update config override

--- config-overrides.js
+++ config-overrides.js
@@ -2,3 +2,21 @@ const { override, addBabelPreset } = require('customize-cra')
 module.exports = override(
   addBabelPreset('@emotion/babel-preset-css-prop')
 )
+
+module.exports = function override(config, env) {
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.oneOf instanceof Array) {
+      return {
+        ...rule,
+        oneOf: [
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+        ]
+      }
+    }
+    return rule;
+  });
+  return config;
+}
