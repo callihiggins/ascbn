fa43aa9b509dde0ae990f12e3700c00e4e8f1fcc
Calli <calli@nyu.edu>
2020-06-25 01:33:12 -0400

    better html

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Nav from '../shared/Nav';
+import InnerHTML from 'dangerously-set-html-content'
 import __html from './ascbn-store-script.html';
+import * as styles from './styles.js';
 
 export const Store = () => 
-  <div dangerouslySetInnerHTML={{__html: __html}} />
+  <div css={styles.containerClass}>
+    <Nav active="store"></Nav>
+    <div css={styles.storeContainerClass} >
+      <InnerHTML html={__html} />
+    </div>
+  </div>
 
 export default Store;
---
+++ src/components/Store/styles.js
@@ -0,0 +1,11 @@
+import { css } from '@emotion/core';
+import { default as theme } from '../../theme';
+import styled from 'styled-components';
+
+export const containerClass = css`
+  background: ${theme.colors.cream};
+`;
+
+export const storeContainerClass = css`
+  padding-top: 80px;
+`;
