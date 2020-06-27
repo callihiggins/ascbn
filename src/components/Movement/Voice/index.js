74a24043aea4a30385ff071743bd30b6c1140869
Calli <calli@nyu.edu>
2020-06-27 13:59:54 -0400

    load more button

--- src/components/Movement/Voice/index.js
+++ src/components/Movement/Voice/index.js
@@ -3,8 +3,7 @@ import InnerHTML from 'dangerously-set-html-content'
 import mapImage from '../../../assets/images/map.png';
 import * as styles from './styles';
 
-
-const html = `<script async src="https://walls.io/js/wallsio-widget-1.2.js" data-wallurl="https://walls.io/n4m7u?nobackground=1&amp;show_header=0" data-title="My social wall" data-width="100%" data-autoheight="1" data-height="800" data-lazyload="1"></script>`;
+const html = `<script async src="https://walls.io/js/wallsio-widget-1.2.js" data-wallurl="https://walls.io/n4m7u?nobackground=1&amp;show_header=0" data-title="My social wall" data-injectloadmorebutton="1" data-loadmoretext="Load More" data-loadmorecount="20" data-width="100%" data-autoheight="1" data-height="800" data-lazyload="1"></script>`;
 export const Voice = () => 
   <>
     <div css={styles.bannerClass}>Speak Up</div>
