582b8a2d1a5e6ff78b22381afa25b189eee0f84a
callihiggins <calli@nyu.edu>
2022-07-27 18:15:04 -0400

    update step into power emails

--- src/components/Home/Watch/index.js
+++ src/components/Home/Watch/index.js
@@ -18,7 +18,7 @@ const Watch = props => (
         <div css={[sharedstyles.hideDesktop, styles.mobileImageContainerClass]}>
           <img src={hostScreeningImage} alt="Host a Screening" />
         </div>
-        <p>Sign up to host a screening or discussion of And She Could Be Next for your friends, neighbors, or community! After signing up, our team will follow-up to share resources and details with you on how to make it happen or help you spread the word. <br /><br />Questions? Email <a href="mailto:stepintopower@andshecouldbenext.com">stepintopower@andshecouldbenext.com</a>.</p>
+        <p>Sign up to host a screening or discussion of And She Could Be Next for your friends, neighbors, or community! After signing up, our team will follow-up to share resources and details with you on how to make it happen or help you spread the word. <br /><br />Questions? Email <a href="mailto:info@andshecouldbenext.com">info@andshecouldbenext.com</a>.</p>
         <div css={styles.buttonGroupClass}>
           <a href="https://airtable.com/shrPwxSFaSKYrKPq5" target="_blank"><button css={styles.buttonClass}>Sign Up</button></a>
         </div>
